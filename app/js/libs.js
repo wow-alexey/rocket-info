2f05eea33fefef1c9a899ec2c328cdde7cee24b1
Alexey Dmitrenko <alexey.dmitrenko@bigdropinc.com>
2019-06-11 14:32:00 +0300

    homepage done

--- app/js/libs.js
+++ app/js/libs.js
@@ -4,3 +4,7 @@ $('.remove-priority').on('click', function (e) {
     e.preventDefault();
     $(this).parent().remove();
 });
+
+$(".burger").on('click', function (e) {
+    $("header").toggleClass("active");
+});
